df86f3def5072b911c8a22e86570eff3f83f9386
Rômulo Zirbes <rzirbes5@rede.ulbra.br>
2024-09-13 14:28:13 -0300

    Ajustando encapsulamento

--- AP1/rede-reconheicmento-numeros.js
+++ AP1/rede-reconheicmento-numeros.js
@@ -4,90 +4,81 @@ class RedeNeural {
         this.tamanhoOculto = tamanhoOculto;
         this.tamanhoSaida = tamanhoSaida;
 
-        // Inicializando pesos aleatórios para a camada oculta e a camada de saída
-        this.pesosEntradaOculto = this.inicializarPesos(tamanhoEntrada, tamanhoOculto);
-        this.pesosOcultoSaida = this.inicializarPesos(tamanhoOculto, tamanhoSaida);
+        this.pesosEntradaOculto = this.#inicializarPesos(tamanhoEntrada, tamanhoOculto);
+        this.pesosOcultoSaida = this.#inicializarPesos(tamanhoOculto, tamanhoSaida);
     }
 
-    inicializarPesos(linhas, colunas) {
-        let pesos = [];
-        for (let i = 0; i < linhas; i++) {
-            let linhaPesos = [];
-            for (let j = 0; j < colunas; j++) {
-                linhaPesos.push(Math.random() * 2 - 1); // valores entre -1 e 1
-            }
-            pesos.push(linhaPesos);
-        }
-        return pesos;
+    // Método privado para inicializar pesos
+    #inicializarPesos(linhas, colunas) {
+        return Array.from({ length: linhas }, () =>
+            Array.from({ length: colunas }, () => Math.random() * 2 - 1)
+        );
     }
 
-    sigmoide(x) {
+    // Função de ativação sigmoide e sua derivada
+    #sigmoide(x) {
         return 1 / (1 + Math.exp(-x));
     }
 
-    derivadaSigmoide(x) {
+    #derivadaSigmoide(x) {
         return x * (1 - x);
     }
 
-    feedforward(entradaMatriz) {
-        let entrada = this.achatar(entradaMatriz);
-
-        // Camada oculta
-        this.saidaOculta = this.multiplicarMatriz(entrada, this.pesosEntradaOculto).map(x => this.sigmoide(x));
-
-        // Camada de saída
-        this.saidaFinal = this.multiplicarMatriz(this.saidaOculta, this.pesosOcultoSaida).map(x => this.sigmoide(x));
-
-        // Arredondar cada valor para 0 ou 1
-        return this.saidaFinal.map(valor => (valor >= 0.90 ? 1 : 0));
+    // Método para propagação da entrada na rede
+    #feedforward(entrada) {
+        const saidaOculta = this.#ativacaoOculta(entrada);
+        const saidaFinal = this.#ativacaoSaida(saidaOculta);
+        return saidaFinal.map((valor) => (valor >= 0.9 ? 1 : 0));
     }
 
-    // Classificação: Selecionar o índice com o maior valor na saída
-    classificar(entradaMatriz) {
-        const saida = this.feedforward(entradaMatriz);
-        // Seleciona o índice do maior valor na saída
-        const numero = saida.indexOf(Math.max(...saida));
-        return `Número ${numero}`;
+    #ativacaoOculta(entrada) {
+        const entradaAchada = this.#achatar(entrada);
+        return this.#multiplicarMatriz(entradaAchada, this.pesosEntradaOculto).map(this.#sigmoide);
     }
 
+    #ativacaoSaida(saidaOculta) {
+        return this.#multiplicarMatriz(saidaOculta, this.pesosOcultoSaida).map(this.#sigmoide);
+    }
+
+    // Função para treinar a rede
     treinar(entradaMatriz, saidaEsperada, taxaAprendizagem) {
-        let entrada = this.achatar(entradaMatriz);
+        const entrada = this.#achatar(entradaMatriz);
+        const saidaOculta = this.#ativacaoOculta(entradaMatriz);
+        const saidaFinal = this.#ativacaoSaida(saidaOculta);
 
-        this.feedforward(entradaMatriz);
+        this.#ajustarPesos(entrada, saidaOculta, saidaFinal, saidaEsperada, taxaAprendizagem);
+    }
 
-        let erroSaida = [];
-        for (let i = 0; i < saidaEsperada.length; i++) {
-            erroSaida.push(saidaEsperada[i] - this.saidaFinal[i]);
-        }
+    #ajustarPesos(entrada, saidaOculta, saidaFinal, saidaEsperada, taxaAprendizagem) {
+        const erroSaida = this.#calcularErro(saidaEsperada, saidaFinal);
+        const ajustesSaida = erroSaida.map((erro, i) => erro * this.#derivadaSigmoide(saidaFinal[i]));
 
-        let ajustesSaida = [];
-        for (let i = 0; i < erroSaida.length; i++) {
-            ajustesSaida.push(erroSaida[i] * this.derivadaSigmoide(this.saidaFinal[i]));
-        }
+        this.#ajustarPesosOcultoSaida(ajustesSaida, saidaOculta, taxaAprendizagem);
+        const erroOculto = this.#calcularErroOculto(ajustesSaida);
+        const ajustesOculto = erroOculto.map((erro, i) => erro * this.#derivadaSigmoide(saidaOculta[i]));
 
-        // Ajuste dos pesos da camada oculta para a camada de saída
+        this.#ajustarPesosEntradaOculto(ajustesOculto, entrada, taxaAprendizagem);
+    }
+
+    #calcularErro(saidaEsperada, saidaFinal) {
+        return saidaEsperada.map((esperada, i) => esperada - saidaFinal[i]);
+    }
+
+    #ajustarPesosOcultoSaida(ajustesSaida, saidaOculta, taxaAprendizagem) {
         for (let i = 0; i < this.pesosOcultoSaida.length; i++) {
             for (let j = 0; j < this.pesosOcultoSaida[i].length; j++) {
-                this.pesosOcultoSaida[i][j] += taxaAprendizagem * ajustesSaida[j] * this.saidaOculta[i];
+                this.pesosOcultoSaida[i][j] += taxaAprendizagem * ajustesSaida[j] * saidaOculta[i];
             }
         }
+    }
 
-        // Calculando o erro da camada oculta
-        let erroOculto = [];
-        for (let i = 0; i < this.pesosEntradaOculto[0].length; i++) {
-            let erro = 0;
-            for (let j = 0; j < ajustesSaida.length; j++) {
-                erro += ajustesSaida[j] * this.pesosOcultoSaida[i][j];
-            }
-            erroOculto.push(erro);
-        }
+    #calcularErroOculto(ajustesSaida) {
+        return this.pesosEntradaOculto[0].map((_, i) =>
+            ajustesSaida.reduce((erro, ajuste, j) => erro + ajuste * this.pesosOcultoSaida[i][j], 0)
+        );
+    }
 
-        // Ajuste dos pesos da camada de entrada para a camada oculta
-        let ajustesOculto = [];
-        for (let i = 0; i < erroOculto.length; i++) {
-            ajustesOculto.push(erroOculto[i] * this.derivadaSigmoide(this.saidaOculta[i]));
-        }
-
+    #ajustarPesosEntradaOculto(ajustesOculto, entrada, taxaAprendizagem) {
         for (let i = 0; i < this.pesosEntradaOculto.length; i++) {
             for (let j = 0; j < this.pesosEntradaOculto[i].length; j++) {
                 this.pesosEntradaOculto[i][j] += taxaAprendizagem * ajustesOculto[j] * entrada[i];
@@ -95,27 +86,24 @@ class RedeNeural {
         }
     }
 
-    multiplicarMatriz(vetor, matriz) {
-        let resultado = [];
-        if (vetor.length !== matriz.length) {
-            throw new Error(`Dimensões incompatíveis: tamanho do vetor (${vetor.length}) e linhas da matriz (${matriz.length})`);
-        }
-
-        for (let j = 0; j < matriz[0].length; j++) {
-            let soma = 0;
-            for (let i = 0; i < vetor.length; i++) {
-                soma += vetor[i] * matriz[i][j];
-            }
-            resultado.push(soma);
-        }
-        return resultado;
+    // Função de classificação
+    classificar(entradaMatriz) {
+        const saida = this.#feedforward(entradaMatriz);
+        const numero = saida.indexOf(Math.max(...saida));
+        return `Número ${numero}`;
     }
 
-    achatar(matriz) {
+    // Funções utilitárias
+    #multiplicarMatriz(vetor, matriz) {
+        return matriz[0].map((_, j) => vetor.reduce((soma, v, i) => soma + v * matriz[i][j], 0));
+    }
+
+    #achatar(matriz) {
         return matriz.flat();
     }
 }
 
+// Exemplo de uso da rede neural
 let rede = new RedeNeural(48, 96, 10);
 
 let treinamento = [
